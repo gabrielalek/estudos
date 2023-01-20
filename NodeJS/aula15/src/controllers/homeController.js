9e24ca64671aa282cd01af0cef9b0bfca39ecd39
gabrielalek <gabrielalex2002@outlook.com>
2023-01-20 05:59:18 -0300

    Aulas sobre injeção de conteúdo e Helmet/CSRF
    Aulas vistas dia 20 de janeiro de 2023

---
+++ NodeJS/aula15/src/controllers/homeController.js
@@ -0,0 +1,18 @@
+// const HomeModel = require('../models/HomeModel');
+// HomeModel.create({
+//     titulo: 'Outra coisa qualquer',
+//     descricao: 'Outra descrição.'
+// })
+//     .then(dados => console.log(dados))
+//     .catch(e => console.log(e));
+
+exports.paginaInicial = (req, res) => {
+    console.log(req.flash('error'), req.flash('success'), req.flash('info'));
+    res.render('index');
+    return;
+};
+
+exports.trataPost = (req, res) => {
+    res.send(req.body);
+    return;
+};
---
+++ NodeJS/aula16/src/middlewares/middleware.js
@@ -0,0 +1,9 @@
+exports.middlewareGlobal = (req, res, next) => {
+    res.locals.umaVariavelLocal = 'Este e o valor da variavel local.';
+    next();
+};
+
+exports.outroMiddleware = (req, res, next) => {
+    console.log('Sou outro middleware');
+    next();
+};
